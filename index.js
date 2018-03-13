1e0c4b741af1462347c33777a4898fee86c75bd4
vincentpelage <vincent.plge@gmail.com>
2018-03-14 09:45:12 +1100

    route test vincent

--- index.js
+++ index.js
@@ -22,6 +22,47 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 
 
 app.use('/', (req, res) => {
+  let urlListMatchTest = 'https://api.dc01.gamelockerapp.com/shards/global/matches?filter[createdAt-start]=2018-03-07T14:25:30Z&page[limit]=5&page[offset]=5&sort=createdAt';
+  let player = 'https://api.dc01.gamelockerapp.com/shards/global/players/951488175786881024';
+  // let urlRosterTest = 'https://api.dc01.gamelockerapp.com/shards/global/rosters/id=6c60ad2c-392b-41c0-a32a-2686715695d9';
+  let config = {};
+  config.headers = {
+    'Accept-Encoding': 'gzip',
+    'Authorization': key,
+    'Accept': 'application/vnd.api+json'
+  }
+
+  axios
+    .get(urlListMatchTest, config)
+    .then((response) => {
+
+      async function GetAllMatches(response){
+        try {
+          const matches = await
+            response.data;
+          return matches;
+        }
+        catch(error) {
+          console.log(error.response)
+        }
+      }
+
+      GetAllMatches(response).then( function(result) {
+          res.json(result);
+      });
+
+    })
+    .catch((error) => {
+      console.log(error.response)
+    });
+
+
+
+
+
+})
+
+app.use('/test-vincent', (req, res) => {
   let urlListMatchTest = 'https://api.dc01.gamelockerapp.com/shards/global/matches?filter[createdAt-start]=2018-03-03T14:25:30Z&page[limit]=5&page[offset]=5&sort=createdAt';
   let player = 'https://api.dc01.gamelockerapp.com/shards/global/players/951488175786881024';
   // let urlRosterTest = 'https://api.dc01.gamelockerapp.com/shards/global/rosters/id=6c60ad2c-392b-41c0-a32a-2686715695d9';
