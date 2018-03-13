d85c562d188cb08e3cf585999cbc7077e4bd46c0
Charlotte Cady <charlottecady@MacBook-Pro-de-Charlotte.local>
2018-03-14 09:14:36 +1100

    fake data

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const axios = require('axios');
 require('dotenv').config();
+const fakeData = require('./fakeData');
 
 // Init variable
 const app = express();
@@ -21,46 +22,45 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 // });
 
 
+// app.use('/', (req, res) => {
+//   let urlListMatchTest = 'https://api.dc01.gamelockerapp.com/shards/global/matches?filter[createdAt-start]=2018-03-03T14:25:30Z&page[limit]=5&page[offset]=5&sort=createdAt';
+//   let config = {};
+//   config.headers = {
+//     'Accept-Encoding': 'gzip',
+//     'Authorization': key,
+//     'Accept': 'application/vnd.api+json'
+//   }
+//
+//   axios
+//     .get(urlListMatchTest, config)
+//     .then((response) => {
+//
+//       async function GetAllMatches(response){
+//         try {
+//           const matches = await
+//             response.data;
+//           return matches;
+//         }
+//         catch(error) {
+//           console.log(error.response)
+//         }
+//       }
+//
+//       GetAllMatches(response).then( function(result) {
+//           res.json(result);
+//       });
+//
+//     })
+//     .catch((error) => {
+//       console.log(error.response)
+//     });
+// });
+
+// Test on fake data
+
 app.use('/', (req, res) => {
-  let urlListMatchTest = 'https://api.dc01.gamelockerapp.com/shards/global/matches?filter[createdAt-start]=2018-03-03T14:25:30Z&page[limit]=5&page[offset]=5&sort=createdAt';
-  let player = 'https://api.dc01.gamelockerapp.com/shards/global/players/951488175786881024';
-  // let urlRosterTest = 'https://api.dc01.gamelockerapp.com/shards/global/rosters/id=6c60ad2c-392b-41c0-a32a-2686715695d9';
-  let config = {};
-  config.headers = {
-    'Accept-Encoding': 'gzip',
-    'Authorization': key,
-    'Accept': 'application/vnd.api+json'
-  }
-
-  axios
-    .get(urlListMatchTest, config)
-    .then((response) => {
-
-      async function GetAllMatches(response){
-        try {
-          const matches = await
-            response.data;
-          return matches;
-        }
-        catch(error) {
-          console.log(error.response)
-        }
-      }
-
-      GetAllMatches(response).then( function(result) {
-          res.json(result);
-      });
-
-    })
-    .catch((error) => {
-      console.log(error.response)
-    });
-
-
-
-
-
-})
+  res.json(fakeData.data);
+});
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(`${__dirname}/client/build/index.html`));
